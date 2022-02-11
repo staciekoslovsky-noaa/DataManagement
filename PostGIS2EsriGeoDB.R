b7eba59dae17513513c05818f5374f0d2b97c4c5
Stacie Hardy <stacie.hardy@noaa.gov>
2022-02-10 17:22:24 -0800

    Update PostGIS2EsriGeoDB.R
    Updated code to include more layers in Esri GeoDB

--- PostGIS2EsriGeoDB.R
+++ PostGIS2EsriGeoDB.R
@@ -33,35 +33,72 @@ con <- RPostgreSQL::dbConnect(PostgreSQL(),
 
 
 # Get data from DB ---------------------------------------------
-### Base layers
+### Base layers --------------------
 # geo_analysis_grid_ak
 analysis_grid_ak <- sf::st_read(con, query = "SELECT * FROM base.geo_analysis_grid_ak") %>%
-  sf::st_transform(4326) %>%
   sf::as_Spatial()
 
-arc.write(file.path(fgdb_path, "base\\geo_analysis_grid_ak"), data = analysis_grid_ak)
+arc.write(file.path(fgdb_path, "base\\geo_analysis_grid_ak"), data = analysis_grid_ak, overwrite = TRUE)
 
-### Coastal harbor seal layers
+# geo_analysis_grid_ak_centroid
+analysis_grid_ak_centroid <- sf::st_read(con, query = "SELECT * FROM base.geo_analysis_grid_ak_centroid") %>%
+  sf::as_Spatial()
+
+arc.write(file.path(fgdb_path, "base\\analysis_grid_ak_centroid"), data = analysis_grid_ak_centroid, overwrite = TRUE)
+
+### Coastal harbor seal layers --------------------
 # geo_polys
 polys <- sf::st_read(con, query = "SELECT * FROM surv_pv_cst.geo_polys") %>%
-  sf::st_transform(4326) %>%
   sf::as_Spatial()
 
-arc.write(file.path(fgdb_path, "surv_pv_cst\\geo_polys"), data = polys)
+arc.write(file.path(fgdb_path, "surv_pv_cst\\geo_polys"), data = polys, overwrite = TRUE)
 
 # geo_haulout
 haulout <- sf::st_read(con, query = "SELECT * FROM surv_pv_cst.geo_haulout", geometry = "geom") %>%
-  sf::st_transform(4326) %>%
   sf::as_Spatial()
 
-arc.write(file.path(fgdb_path, "surv_pv_cst\\geo_haulout"), data = haulout)
+arc.write(file.path(fgdb_path, "surv_pv_cst\\geo_haulout"), data = haulout, overwrite = TRUE)
 
 # geo_abundance_4agol
 abundance_4agol <- sf::st_read(con, query = "SELECT * FROM surv_pv_cst.geo_abundance_4agol") %>%
-  sf::st_transform(4326) %>%
   sf::as_Spatial()
 
-arc.write(file.path(fgdb_path, "surv_pv_cst\\geo_abundance_4agol"), data = abundance_4agol)
+arc.write(file.path(fgdb_path, "surv_pv_cst\\geo_abundance_4agol"), data = abundance_4agol, overwrite = TRUE)
 
-### Glacial harbor seal layers
+### Glacial harbor seal layers --------------------
 
+
+### Stock layers --------------------
+# geo_dist_pv -- THROWING ERROR WHEN IMPORTING>>>>>>NEED TO REVISIT THIS ONE!!!!!!!!!!!
+# geo_dist_pv <- sf::st_read(con, query = "SELECT * FROM stock.geo_dist_pv") %>%
+#   sf::as_Spatial()
+# 
+# arc.write(file.path(fgdb_path, "stock\\geo_dist_pv"), data = geo_dist_pv, overwrite = TRUE)
+
+# geo_dist_bd
+geo_dist_bd <- sf::st_read(con, query = "SELECT * FROM stock.geo_dist_bd") %>%
+  sf::as_Spatial()
+
+arc.write(file.path(fgdb_path, "stock\\geo_dist_bd"), data = geo_dist_bd, overwrite = TRUE)
+
+# geo_dist_rd
+geo_dist_rd <- sf::st_read(con, query = "SELECT * FROM stock.geo_dist_rd") %>%
+  sf::as_Spatial()
+
+arc.write(file.path(fgdb_path, "stock\\geo_dist_rd"), data = geo_dist_rd, overwrite = TRUE)
+
+# geo_dist_rn
+geo_dist_rn <- sf::st_read(con, query = "SELECT * FROM stock.geo_dist_rn") %>%
+  sf::as_Spatial()
+
+arc.write(file.path(fgdb_path, "stock\\geo_dist_rn"), data = geo_dist_rn, overwrite = TRUE)
+
+# geo_dist_sd
+geo_dist_sd <- sf::st_read(con, query = "SELECT * FROM stock.geo_dist_sd") %>%
+  sf::as_Spatial()
+
+arc.write(file.path(fgdb_path, "stock\\geo_dist_sd"), data = geo_dist_sd, overwrite = TRUE)
+
+# Disconnect from DB  --------------------------------------
+RPostgreSQL::dbDisconnect(con)
+rm(con)
