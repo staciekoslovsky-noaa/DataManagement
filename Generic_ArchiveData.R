439b636ef716335b0c27b49729745c8fb9143b63
staciekoslovsky-noaa <stacie.koslovsky@noaa.gov>
2024-08-28 12:02:54 -0700

    Create Generic_ArchiveData.R

---
+++ Generic_ArchiveData.R
@@ -0,0 +1,1 @@
+file.copy("D:\\HARBORSEAL_2019", "\\\\akc0ss-n086\\NMML_Polar_Imagery\\Surveys_HS\\Glacial\\Projects\\Surveys Glacial Sites Counts\\_FromJamie_TO_BE_ARCHIVED", overwrite = FALSE, recursive = TRUE)
